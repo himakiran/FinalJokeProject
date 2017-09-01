d71b695260a5f76d408d00f4894cf04599a6c5d5
himakiran <himakiran@gmail.com>
2017-09-01 14:43:01 +0530

    Implemented GCE server locally and integrated it with the app successfully.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,20 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import com.google.android.gms.ads.MobileAds;
+
 import com.udacity.gradle.javajokes.JavaJokes;
 import com.udacity.gradle.jokedisplaylibrary.JokeActivity;
-
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         String joke = javaJokes.getJoke();
         intent.putExtra(JokeActivity.JOKE_KEY, joke);
         startActivity(intent);
+        new EndpointsAsyncTask().execute(new Pair<Context,String>(this,"Himakiran"));
     }
 
 
